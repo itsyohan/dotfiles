77af225252e2367921c87db724a5a1b5154516ff
Yohan Lee <io.yohanlee@gmail.com>
2023-10-13 22:12:53 +0100

    add neovim setup

---
+++ bin/nvim.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+read -p "This will blow away existing neovim configs, continue? (y/n)" yn
+
+if [[ ! $yn =~ ^[Yy].* ]]; then
+    echo "Back up your configs and try again:"
+    echo "mv ~/.config/nvim ~/.config/nvim.bak"
+    echo "mv ~/.local/share/nvim ~/.local/share/nvim.bak"
+    exit
+fi
+
+rm -rf ~/.config/nvim
+rm -rf ~/.local/share/nvim
+
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+git clone https://github.com/itsyohan/AstroNvimConfig ~/.config/nvim/lua/user
